e3c4472ab24be75c6e52701a3d68a31886502086
Theresa Neate <theresa.neate@rea-group.com>
2019-09-22 20:23:50 +1000

    First commit

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,25 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+
+
+    public static void main(String[] args) {
+//      Get the number of hours worked
+        System.out.println("Enter the number of hours the employee worked: ");
+        Scanner scanner = new Scanner(System.in);
+        int hoursWorked = scanner.nextInt();
+
+//      Get the hourly pay rate
+        System.out.println("enter the employee's pay rate: ");
+        double payRate = scanner.nextDouble();
+
+//      Multiply hours and pay rate
+        double pay = payRate * hoursWorked;
+
+//      Display result
+        System.out.println("The employee is receiving $" + pay );
+
+    }
+}
